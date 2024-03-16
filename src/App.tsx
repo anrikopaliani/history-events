c597d7aec6797c2bbf53d8e4e709f79426aada16
kopala123 <anri.kopaliani.1@btu.edu.ge>
2024-03-16 16:52:27 +0400

    feat:create new icon for marker

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-// import { Icon } from "leaflet";
+import { Icon } from "leaflet";
 
 const defaultPosition: [number, number] = [51.505, -0.09];
 
 function App() {
+  const icon: Icon = new Icon({
+    iconUrl: "marker.svg",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+  });
   return (
     <>
       <main className="content">
@@ -13,11 +18,10 @@ function App() {
           <MapContainer
             zoom={13}
             center={defaultPosition}
-            // className="map-container"
-            style={{ width: "100%", height: "100%" }}
+            className="map-container"
           >
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></TileLayer>
-            <Marker position={defaultPosition}>
+            <Marker position={defaultPosition} icon={icon}>
               <Popup>ds</Popup>
             </Marker>
           </MapContainer>
